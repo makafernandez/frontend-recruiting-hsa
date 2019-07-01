5c0a2f360b37352b158ef7246ed21601ee4770a0
makafernandez <mfernanlav@gmail.com>
2019-07-01 03:10:29 -0400

    feat(SearchBar): crea componente para barra de búsqueda

---
+++ src/app/search/search-bar/search-bar/search-bar.component.ts
@@ -0,0 +1,19 @@
+import { SearchComponent } from './../../../modules/search/search/search.component';
+import { Component, OnInit, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss']
+})
+export class SearchBarComponent implements OnInit {
+  @Input() clickEvent: SearchComponent;
+  @Input() placeholder: SearchComponent;
+  @Output() searchTerm = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
